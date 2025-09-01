2e5241e8581eb03e3c1cb31764d3dc42f4b4303b
Md. Rakibul Islam Sabid <md.rakibulislamsabid179@gmail.com>
2025-09-01 23:17:45 +0600

    created contests app

--- uiucpcoj/settings.py
+++ uiucpcoj/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     # new apps
     "core",
     "users",
+    "contests",
 ]
 
 MIDDLEWARE = [
--- uiucpcoj/urls.py
+++ uiucpcoj/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("users/", include("users.urls")),
+    path("contests/", include("contests.urls")),
 ]
